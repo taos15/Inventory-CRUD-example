0df686c2e497af7d876de507953424b9da9a197e
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-04-07 20:48:46 -0400

    feat: - added delete buttom for the items

--- ui/src/components/InventoryCard.jsx
+++ ui/src/components/InventoryCard.jsx
@@ -1,11 +1,46 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { Col, Row } from 'react-bootstrap';
+import axios from 'axios';
+import { useState } from 'react';
+import { Alert, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useLocation, useNavigate } from 'react-router-dom';
+
+import { useSome } from '../utilities/MainContextProvider';
 
 export default function InventoryCard({ item }) {
   const navigateTo = useNavigate();
   const location = useLocation();
+
+  const [userError, setUserError] = useState(false);
+  const [userExist, setUserExist] = useState('');
+
+  // context
+  const { isLoggedIn, setIsLoggedIn, currentUser, setCurrentUser } = useSome();
+
+  // delete handlers
+  const handleData = (data) => {
+    axios
+      .delete(`http://localhost:5010/api/v1/item/${item.item_name}`)
+      .then((res) => navigateTo('/myinventory'))
+      .catch((err) => {
+        setUserError(true);
+        console.log(err.response.data);
+        console.log(userError);
+        setUserExist(err.response.data);
+      });
+  };
+
+  if (userError) {
+    return (
+      <Alert key='danger' variant='danger'>
+        {`${userExist} `}
+        <button onClick={() => setUserError(false)} type='button'>
+          Try again!
+        </button>
+      </Alert>
+    );
+  }
 
   return (
     <Col>
@@ -28,6 +63,11 @@ export default function InventoryCard({ item }) {
               : item.description}
           </Card.Text>
           <Card.Text className='text-muted mb-2'>{`Quantity: ${item.quantity}`}</Card.Text>
+          {isLoggedIn && location.pathname.includes('/item') && (
+            <Button variant='primary' onClick={() => handleData()}>
+              Delete
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </Col>
